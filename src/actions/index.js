9208da2ba9f48d77542c5834e766197a42e4ae99
John Hannah <john@friendlymachine.net>
2017-12-29 14:38:25 -0500

    package updates, new thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,14 @@
 import axios from 'axios';
+
 export const FETCH_POSTS = 'FETCH_POSTS';
+
+export const fetchPostsIfNeeded = () => (dispatch, getState) => {
+  const state = getState();
+  console.log('STATE', state);
+  if (state.posts === undefined) {
+    dispatch(fetchPosts());
+  }
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await  axios('https://jsonplaceholder.typicode.com/posts');
